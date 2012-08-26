1d49373044c02cbc650ac08b01c9cad04d1d1b3b
Travis Bybee <travis.bybee@gmail.com>
2012-08-26 13:30:18 -0600

    Updated application to use the entire screen as a button whose label is update with the current date and time when it is pushed.
    I didn't understand why the button takes up the entire screen and why
    the the XML layout we had previously defined no longer existed in the
    app until I took a look at the pre-genereated source code that was
    pasted over for this exercise. The setContentView is being feed the
    button as the view for the app, instead of the information in
    R.layout.activity_hello_android.
    over for this

--- HelloAndroid/src/csci498/hello/HelloAndroid.java
+++ HelloAndroid/src/csci498/hello/HelloAndroid.java
@@ -1,20 +1,28 @@
 package csci498.hello;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.util.Date;
 
-public class HelloAndroid extends Activity {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello_android);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_hello_android, menu);
-        return true;
-    }
+public class HelloAndroid extends Activity implements View.OnClickListener 
+{ 
+	Button btn;
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		btn=new Button(this); 
+		btn.setOnClickListener(this); 
+		updateTime(); 
+		setContentView(btn);
+	}
+	public void onClick(View view) 
+	{
+		updateTime(); 
+	}
+	private void updateTime() 
+	{
+		btn.setText(new Date().toString()); 
+	}
 }
