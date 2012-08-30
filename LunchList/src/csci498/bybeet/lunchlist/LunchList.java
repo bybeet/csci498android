ce953cb93ba04694e26ce2dcb9d219c0c281893d
Travis Bybee <travis.bybee@gmail.com>
2012-08-30 00:59:45 -0600

    NOTE: Past couple of commits have been Tutorial 4, not Tutorial 3. I don't know how to edit commit messages....
    Tutorial 5 complete. Modified the list of stored restaurants to be more
    useful by implementing another XML layout for each row in the list.
    Icons added to the drawable folder to help with visually displaying
    information.

--- LunchList/src/csci498/bybeet/lunchlist/LunchList.java
+++ LunchList/src/csci498/bybeet/lunchlist/LunchList.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -18,31 +22,91 @@ import android.widget.TableLayout;
 import android.widget.TableLayout.LayoutParams;
 
 public class LunchList extends Activity {
+	
+	static class RestaurantHolder
+	{
+		private TextView name = null;
+		private TextView address = null;
+		private ImageView icon = null;
+		
+		RestaurantHolder (View row)
+		{
+			name = (TextView)row.findViewById(R.id.title);
+			address = (TextView)row.findViewById(R.id.address);
+			icon = (ImageView)row.findViewById(R.id.icon);
+		}
+		
+		void populateFrom (Restaurant r)
+		{
+			name.setText(r.getName());
+			address.setText(r.getAddress());
+			
+			if (r.getType().equals("sit_down")) 
+			{ 
+				icon.setImageResource(R.drawable.ball_red);
+			}
+			else if (r.getType().equals("take_out")) 
+			{
+				icon.setImageResource(R.drawable.ball_yellow); 
+			}
+			else 
+			{ 
+				icon.setImageResource(R.drawable.ball_green);
+			}
+		}
+	}
+
+	class RestaurantAdapter extends ArrayAdapter<Restaurant>{
+		RestaurantAdapter(){
+			super(LunchList.this, android.R.layout.simple_list_item_1, restaurants);
+		}
+		
+		public View getView(int position, View convertView, ViewGroup parent)
+		{
+			View row = convertView;
+			RestaurantHolder holder = null;
+			
+			if( row == null )
+			{
+				LayoutInflater inflater = getLayoutInflater();
+				row = inflater.inflate(R.layout.row, null);
+				holder = new RestaurantHolder(row);
+				row.setTag(holder);
+			}
+			else
+			{
+				holder = (RestaurantHolder)row.getTag();
+			}
+			
+			holder.populateFrom(restaurants.get(position));
+			return(row);
+		}
+	}
 
 	List<Restaurant> restaurants = new ArrayList<Restaurant> ();
-	ArrayAdapter<Restaurant> adapter = null;
+	RestaurantAdapter adapter = null;
 	ArrayAdapter<String> adapterAddress = null;
 	List<String> addresses = new ArrayList<String> ();
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lunch_list);
-        
-        Button save = (Button)findViewById(R.id.save);
-        save.setOnClickListener(onSave);
-        
-        ListView list = (ListView)findViewById(R.id.restaurants);
-        AutoCompleteTextView textView = (AutoCompleteTextView)findViewById(R.id.addr);
-        
-        adapter = new ArrayAdapter<Restaurant>(this, android.R.layout.simple_list_item_1, restaurants);
-        list.setAdapter(adapter);
-        
-        adapterAddress = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, addresses);
-        textView.setAdapter(adapterAddress);
-        
-        //Radio button group in Java code.
-        /*
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lunch_list);
+
+		Button save = (Button)findViewById(R.id.save);
+		save.setOnClickListener(onSave);
+
+		ListView list = (ListView)findViewById(R.id.restaurants);
+		AutoCompleteTextView textView = (AutoCompleteTextView)findViewById(R.id.addr);
+
+		adapter = new RestaurantAdapter();
+		list.setAdapter(adapter);
+
+		adapterAddress = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, addresses);
+		textView.setAdapter(adapterAddress);
+
+		//Radio button group in Java code.
+		/*
         RadioButton sitDown = new RadioButton(this);
         sitDown.setText("Sit-Down");
         RadioButton takeOut = new RadioButton(this);
@@ -53,29 +117,29 @@ public class LunchList extends Activity {
         typesGroup.addView( sitDown , 0);
         typesGroup.addView( takeOut, 1);
         typesGroup.addView( delivery, 2);
-        
+
         addContentView(typesGroup, new LayoutParams( R.layout.activity_lunch_list , 0));
         setContentView(typesGroup);     
-        */
-    }
+		 */
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_lunch_list, menu);
-        return true;
-    }
-    
-    private View.OnClickListener onSave = new View.OnClickListener() {
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_lunch_list, menu);
+		return true;
+	}
+
+	private View.OnClickListener onSave = new View.OnClickListener() {
 		@Override
 		public void onClick(View v) {
 			EditText name = (EditText)findViewById(R.id.name);
 			EditText address = (EditText)findViewById(R.id.addr);
 			RadioGroup types = (RadioGroup)findViewById(R.id.types);
-			
+
 			Restaurant restaurant = new Restaurant();
 			restaurant.setAddress(address.getText().toString());
 			restaurant.setName(name.getText().toString());
-			
+
 			switch(types.getCheckedRadioButtonId())
 			{
 			case R.id.sit_down:
@@ -88,14 +152,14 @@ public class LunchList extends Activity {
 				restaurant.setType("delivery");
 				break;
 			}
-			
+
 			adapterAddress.add(address.getText().toString());
 			adapter.add(restaurant);
-			
+
 			name.setText(null);
 			address.setText(null);
-			
-			
+
+
 		}
 	};
 }
