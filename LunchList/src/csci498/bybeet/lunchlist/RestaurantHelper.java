eae2db48ffbba8973dbe3b0b0211972d8b994b3a
Travis Bybee <travis.bybee@gmail.com>
2012-10-01 18:07:10 -0600

    onCreate() method for RestaurantHelper

--- LunchList/src/csci498/bybeet/lunchlist/RestaurantHelper.java
+++ LunchList/src/csci498/bybeet/lunchlist/RestaurantHelper.java
@@ -14,9 +14,13 @@ public class RestaurantHelper extends SQLiteOpenHelper {
 	}
 	
 	@Override
-	public void onCreate (SQLiteDatabase db) {}
+	public void onCreate (SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT);");
+	}
 	
 	@Override
-	public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion) {}
+	public void onUpgrade (SQLiteDatabase db, int oldVersion, int newVersion) {
+		//Not currently needed, not called until 2nd db schema
+	}
 	
 }
