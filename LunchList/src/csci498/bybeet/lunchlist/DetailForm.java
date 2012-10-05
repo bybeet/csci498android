61a2c2006925b203f2769d4c31896d63cab06362
Travis Bybee <travis.bybee@gmail.com>
2012-10-05 01:01:38 -0600

    Form entries are reinserted to the form after device rotation. onRestoreInstanceState() is responsible for this.

--- LunchList/src/csci498/bybeet/lunchlist/DetailForm.java
+++ LunchList/src/csci498/bybeet/lunchlist/DetailForm.java
@@ -54,7 +54,16 @@ public class DetailForm extends Activity {
 		state.putString("address", address.getText().toString());
 		state.putString("notes", notes.getText().toString());
 		state.putInt("type", types.getCheckedRadioButtonId());
-
+	}
+	
+	@Override
+	public void onRestoreInstanceState(Bundle state) {
+		super.onRestoreInstanceState(state);
+		
+		name.setText(state.getString("name"));
+		address.setText(state.getString("address"));
+		notes.setText(state.getString("notes"));
+		types.check(state.getInt("type"));
 	}
 
 	private View.OnClickListener onSave = new View.OnClickListener() { 
