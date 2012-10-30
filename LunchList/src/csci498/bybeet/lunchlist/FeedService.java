999457de794a157bbb2d2343c5fb8f83f4ef7e6f
Travis Bybee <travis.bybee@gmail.com>
2012-10-30 15:34:30 -0600

    Added an RSSReader and RSSFeed to the FeedService onHandleIntent().

--- LunchList/src/csci498/bybeet/lunchlist/FeedService.java
+++ LunchList/src/csci498/bybeet/lunchlist/FeedService.java
@@ -1,9 +1,15 @@
 package csci498.bybeet.lunchlist;
 
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class FeedService extends IntentService {
+	
+	public static final String EXTRA_URL = "apt.tutorial.EXTRA_URL";
 	
 	public FeedService() {
 		super("FeedService");
@@ -11,6 +17,13 @@ public class FeedService extends IntentService {
 	
 	@Override
 	public void onHandleIntent(Intent i) {
-		//do something
+		RSSReader reader = new RSSReader();
+		
+		try {
+			RSSFeed results = reader.load(i.getStringExtra(EXTRA_URL));
+		}
+		catch(Exception e) {
+			Log.e("LunchList", "Exception parsing feed", e);
+		}
 	}
 }
