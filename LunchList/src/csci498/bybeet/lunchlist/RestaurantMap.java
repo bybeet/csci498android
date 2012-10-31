81931061e27d5a35c4b1a677193499ff69c00c0d
Travis Bybee <travis.bybee@gmail.com>
2012-10-30 21:23:15 -0600

    Added RestaurantOverlay inner class for custom map overlay.

--- LunchList/src/csci498/bybeet/lunchlist/RestaurantMap.java
+++ LunchList/src/csci498/bybeet/lunchlist/RestaurantMap.java
@@ -1,10 +1,13 @@
 package csci498.bybeet.lunchlist;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
 
 public class RestaurantMap extends MapActivity {
 
@@ -28,11 +31,37 @@ public class RestaurantMap extends MapActivity {
 		GeoPoint status = new GeoPoint((int)(latitude*1000000.0),(int)(longitude*1000000.0));
 		map.getController().setCenter(status);
 		map.setBuiltInZoomControls(true);
+		
+		Drawable marker = getResources().getDrawable(R.drawable.marker);
+		map.getOverlays().add(new RestaurantOverlay(marker, status, getIntent().getStringExtra(EXTRA_NAME)));
 	}
 	
 	@Override
 	public boolean isRouteDisplayed(){
 		return false;
+	}
+	
+	private class RestaurantOverlay extends ItemizedOverlay<OverlayItem> {
+		
+		private OverlayItem item;
+		
+		public RestaurantOverlay(Drawable marker, GeoPoint point, String name) {
+			super(marker);
+			boundCenterBottom(marker);
+			item = new OverlayItem(point, name, name);
+			populate();
+		}
+		
+		@Override
+		protected OverlayItem createItem(int i) {
+			return item;
+		}
+		
+		@Override
+		public int size() {
+			return 1;
+		}
+	
 	}
 	
 }
