ba33f1b9feeac40d479f47bcc55da10fbb0c961d
Travis Bybee <travis.bybee@gmail.com>
2012-11-26 23:07:41 -0700

    Modified AppWidget to call widget helper class onUpdate.

--- LunchList/src/csci498/bybeet/lunchlist/AppWidget.java
+++ LunchList/src/csci498/bybeet/lunchlist/AppWidget.java
@@ -1,7 +1,15 @@
 package csci498.bybeet.lunchlist;
 
-import android.appwidget.*;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
 
 public class AppWidget extends AppWidgetProvider {
+
+	@Override
+	public void onUpdate(Context ctxt, AppWidgetManager mgr, int[] appWidgetIds){
+		ctxt.startService(new Intent(ctxt, WidgetService.class));
+	}
 	
 }
